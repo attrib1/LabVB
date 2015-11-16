3dedeab5008969436a873e1664977d3f71576f53
com_5202 <you@example.com>
2015-11-16 14:51:18 +0700

    Week12

---
+++ Lab12/CrystalReportsApplication2/CrystalReportsApplication2/My Project/AssemblyInfo.vb
@@ -0,0 +1,17 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("CrystalReportsApplication2")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("CrystalReportsApplication2")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("759aed1f-6161-448a-bdfc-aa9982a74087")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
